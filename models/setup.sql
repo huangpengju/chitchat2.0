e13e3236affe7e45ab7ce6303282afc0f54fa2e2
huangpengju <15713716933@163.com>
2023-09-12 21:54:55 +0800

    优化sql

--- models/setup.sql
+++ models/setup.sql
@@ -2,14 +2,14 @@ CREATE DATABASE IF NOT EXISTS chitchat DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_u
 
 CREATE TABLE `cc_user`(
     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-    `username` varchar(50);unique NOT NULL COMMENT '用户名',
+    `username` varchar(50) unique NOT NULL COMMENT '用户名',
     `password` varchar(100) NOT NULL COMMENT '密码',
     `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
     `phone` varchar(20) DEFAULT NULL COMMENT '电话',
     `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `username_UNIQUE` (`username`)
-)ENGINE=InoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';  
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';  
 
 CREATE TABLE `cc_user_info`(
     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户信息ID',
@@ -23,4 +23,4 @@ CREATE TABLE `cc_user_info`(
     PRIMARY KEY (`id`),
     KEY `FK_user` (`user_id`),
     CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE 
-)ENGINE=InoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
